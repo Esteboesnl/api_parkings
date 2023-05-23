48d3c7104658ec4d7d74e0025fe20aaaf43746d6
unknown <loleste.lolo@gmail.com>
2023-05-23 12:12:02 +0200

    Fin Partie 1

--- index.js
+++ index.js
@@ -3,39 +3,34 @@ const app = express()
 const parkings = require('./parkings.json')
 // Middleware
 app.use(express.json())
-
-
-
-// Route
+// Liste de tous les parkings
 app.get('/parkings', (req, res) => { res.status(200).json(parkings) })
+// Données d'un parking particulier
 app.get('/parkings/:id', (req, res) => {
 const id = parseInt(req.params.id)
 const parking = parkings.find(parking => parking.id === id)
 res.status(200).json(parking)
 })
-
-
+// Création d'un parking
 app.post('/parkings', (req, res) => {
-    console.log(req.body) // Uniquement pendant la phase de dev
-    parkings.push(req.body) // Les données insérées seront passées dans le body de la
-    requête
-    res.status(200).json(parkings)
-    })
-
+parkings.push(req.body)
+res.status(200).json(parkings)
+})
+// Modification d'un parking particulier
 app.put('/parkings/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parking.name = req.body.name,
-    parking.city = req.body.city,
-    parking.type = req.body.type,
-    res.status(200).json(parking)
-    })
-
+const id = parseInt(req.params.id)
+let parking = parkings.find(parking => parking.id === id)
+parking.name = req.body.name,
+parking.city = req.body.city,
+parking.type = req.body.type,
+res.status(200).json(parking)
+})
+// Suppression d'un parking particulier
 app.delete('/parkings/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parkings.splice(parkings.indexOf(parking), 1)
-    res.status(200).json(parkings)
-    })
-
+const id = parseInt(req.params.id)
+let parking = parkings.find(parking => parking.id === id)
+parkings.splice(parkings.indexOf(parking), 1)
+res.status(200).json(parkings)
+})
+// Mise en écoute du serveur
 app.listen(8080, () => { console.log("Serveur à l'écoute") })
